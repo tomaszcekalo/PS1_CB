d33debe778ef325ff775655aea44e48f907801d8
Tomasz Cekało <tomasz.cekalo@gmail.com>
2025-03-01 11:06:58 +0100

    Added SQLite

--- PS1_CB/Program.cs
+++ PS1_CB/Program.cs
@@ -6,8 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+
+builder.Services.AddSqlite<ApplicationDbContext>("Data Source=.\\messages.db");
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
